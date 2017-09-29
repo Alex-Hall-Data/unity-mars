045e30a2036fd5c60c50a2ba0301683559124193
Alex Hall <northern.bantam@gmail.com>
2017-09-29 18:55:55 +0100

    Procedural terrain generation scene added

---
+++ Assets/Scripts/TerrainGeneration/TerrainChunkSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TerrainGenerator
+{
+
+public class TerrainChunkSettings:MonoBehaviour  {
+
+	//these define resolution - must be power of two plus one (start with 129)
+	public int HeightMapResolution { get; private set;}
+	public int AlphaMapResolution{ get; private set; }
+
+	public int Length{ get; private set; }//length of chunk
+	public int Height{ get; private set; }//max terrain height of chunk
+
+
+	public TerrainChunkSettings (int heightMapResolution, int alphaMapResolution, int length, int height ){
+		HeightMapResolution=heightMapResolution;
+		AlphaMapResolution=alphaMapResolution;
+		Length=length;
+		Height=height;
+	}
+}
+}
---
+++ Assets/terraintest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TerrainGenerator{
+
+public class terraintest : MonoBehaviour {
+
+	private TerrainChunk terrainGenerator;
+
+	// Use this for initialization
+	void Start () {
+		terrainGenerator = GetComponent<TerrainChunk> ();
+			terrainGenerator.Test ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+}
